8adce1dd780db635bfae1c3bd7f708e606738207
wewilliams9999 <williamswallise@gmail.com>
2022-03-05 18:14:43 -0600

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit (double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account!");
+            _balance = amount;
+        }
+
+        public double GetBalance ()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit today?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+           
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance,0:c}");
+        }
+    }
+}
